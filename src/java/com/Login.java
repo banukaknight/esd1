1091f9bb768c16b639e4c8b0a0d9ebab5f2363f2
Leo Edermaniger <33464117+LeoEdermaniger@users.noreply.github.com>
2017-11-12 16:02:47 +0000

    Login added

---
+++ src/java/com/Login.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author leoed
+ */
+public class Login {
+    public static User attemptLogin(String userName, String password, DBBean bean){
+        ArrayList<User> users = new ArrayList<>();
+        try {
+            users = bean.getUsers();
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for(User u : users){
+            if(u.id.equalsIgnoreCase(userName))
+                if(u.password.equals(password))
+                    return u;
+        }
+        return null;
+    }
+}
---
+++ web/login.jsp
@@ -0,0 +1,31 @@
+<%-- 
+    Document   : login
+    Created on : 12-Nov-2017, 15:48:18
+    Author     : leoed
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form action="LoginServlet" method="post">  
+        Name:<input type="text" name="name"><br>  
+        Password:<input type="password" name="password"><br>  
+        <input type="submit" value="login">  
+    </form>
+    <p>
+    
+    <%
+        String s = (String)request.getAttribute("message");
+        
+        if(s != null)
+            out.print(s);
+    %>
+    
+    </body>
+</html>
