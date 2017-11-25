1979ed79e61b11ea04c51e5e70030f64fd1657ce
Leo Edermaniger <33464117+LeoEdermaniger@users.noreply.github.com>
2017-11-25 06:49:10 +0000

    Approve Memberships

---
+++ src/java/com/Members.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author leoed
+ */
+public class Members {
+    public static ArrayList<Member> getAppliedMembers(DBBean bean) throws SQLException{
+        ArrayList<Member> appliedMembers = new ArrayList<>();
+        ArrayList<Member> allMembers = bean.getMembers();
+        
+        for(Member m : allMembers){
+            if(m.status.equalsIgnoreCase("APPLIED"))
+                appliedMembers.add(m);
+        }
+        
+        return appliedMembers;
+    }
+    
+    public static void approveMember(Member m, DBBean bean){
+        m.status = "APPROVED";
+        bean.updateMember(m);
+    }
+}
---
+++ web/ApproveMembers.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : ApproveMembers
+    Created on : 25-Nov-2017, 06:32:53
+    Author     : leoed
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Approve Memberships</title>
+    </head>
+    <body>
+        <div id="checkboxes">
+            <label>Unapproved Memberships</label>
+            <ul>
+              <li><input type="checkbox"> checkbox 1</li>
+              <li><input type="checkbox"> checkbox 2</li>
+              <li><input type="checkbox"> checkbox 3</li>
+              <li><input type="checkbox"> checkbox 4</li>
+            </ul>
+          </div>
+    </body>
+</html>
